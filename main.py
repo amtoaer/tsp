f1e0a53a1e4e03d12048fac798cb5dc7e8a0cd1d
amtoaer <b1361974998@gmail.com>
2020-07-07 17:47:20 +0800

    feat: 对数据集的距离类型进行统计

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from utils import utils
+import os
+import re
 
 
 def main():
-    for line in utils.readFile('a280.tsp'):
-        print(line, end='')
+    files = os.listdir('lib')
+    length = len(files)
+    count = dict()
+    toMatch = 'EDGE_WEIGHT_TYPE[ ]{0,1}:(.+)$'
+    for file in files:
+        for line in utils.readFile(file):
+            result = re.match(toMatch, line)
+            if result != None:
+                weight_type = result.groups()[0]
+                if weight_type in count.keys():
+                    count[weight_type] += 1
+                else:
+                    count[weight_type] = 1
+    print('总共有{}个文件'.format(length))
+    print('距离类型如下：')
+    print(count)
 
 
 if __name__ == '__main__':
--- utils/utils.py
+++ utils/utils.py
@@ -3,11 +3,11 @@ import os.path
 
 
 def readFile(name):
-    path = os.path.join('lib', name+'.gz')
+    path = os.path.join('lib', name)
     if os.path.exists(path):
         with gzip.open(path, 'r') as file:
             for line in file:
                 yield line.decode('utf-8')
     else:
-        print('path is not existed')
+        print('file does not exist')
         exit(1)
