c0ead6294f3887715f7b798e3eeda77a45b7b4d1
zimi-star <2947289938@qq.com>
2020-07-17 10:10:29 +0800

    add GA

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils import io
-from algo import nearest_neighbor, greedy, opt
+from algo import nearest_neighbor, greedy, opt, GA
 from os import listdir
+
 
 
 def main():
@@ -10,6 +11,8 @@ def main():
             io.getData(file), file.split('.')[0]).operate()
         greedy.greedy(io.getData(file), file.split('.')[0]).operate()
         opt.opt(io.getData(file), file.split('.')[0]).opt2()
+        GA.GA(io.getData(file), file.split('.')[0]).find_best_path()
+
 
 
 if __name__ == '__main__':
